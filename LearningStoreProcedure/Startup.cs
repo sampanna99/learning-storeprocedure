241a7dfbab39d30989f60a06222df43700337058
sampanna pokhrel <sampanna99@gmail.com>
2016-12-04 14:17:22 -0800

    Add project files.

---
+++ LearningStoreProcedure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningStoreProcedure.Startup))]
+namespace LearningStoreProcedure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
